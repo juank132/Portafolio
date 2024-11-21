cac1b4d78f4ac69d2b0ff4de4414b0bc75cb5926
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-11-21 20:35:37 -0300

    semi

--- front/src/components/Carrusel/Carrusel.tsx
+++ front/src/components/Carrusel/Carrusel.tsx
@@ -5,8 +5,13 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Card from "../Cards/CardsCarrusel";
 
+interface ArrowProps {
+  className: string;
+  onClick: () => void;
+}
+
 // Botón personalizado "Prev"
-const PrevArrow = (props: any) => {
+const PrevArrow = (props: ArrowProps) => {
   const { className, onClick } = props;
   return (
     <button
@@ -19,7 +24,7 @@ const PrevArrow = (props: any) => {
 };
 
 // Botón personalizado "Next"
-const NextArrow = (props: any) => {
+const NextArrow = (props: ArrowProps) => {
   const { className, onClick } = props;
   return (
     <button
@@ -31,15 +36,23 @@ const NextArrow = (props: any) => {
   );
 };
 
+interface Producto {
+  id: number;
+  name: string;
+  src: string;
+  alt: string;
+  href: string;
+}
+
 export const Carrusel = () => {
-  const [productos, setProductos] = useState<any[]>([]);
+  const [productos, setProductos] = useState<Producto[]>([]);
 
   // const fetchProductos = () => {
     
   // };
 
   useEffect(() => {
-    const data = [{name: "5tart Travel",src: "https://res.cloudinary.com/dfktz8zkt/image/upload/v1728428007/cbey16iyadftprvqda3k.png" , alt: "Proyecto grupal: desarrollo de un marketplace especializado en la compra y venta de viajes. La plataforma permite a los usuarios explorar, publicar y adquirir experiencias de viaje, facilitando la conexión entre agencias y viajeros de manera eficiente e intuitiva.", href: "https://5tart-travel-front-w2ip.vercel.app/" },{name: "Las Divas De Romi",src: "https://res.cloudinary.com/dfktz8zkt/image/upload/v1732223080/ynlk1fioljxs2krzlhq3.png", alt:"Es un eCommerce de productos y cursos online una plataforma que permite la compra de productos físicos y digitales, como productos, así como el acceso a cursos en línea en diversas áreas. Los usuarios pueden comprar de manera fácil y segura, con pagos electrónicos y acceso inmediato a los cursos. (EN PROCESO)", href: "https://personal-woad-phi.vercel.app/" },]
+    const data = [{id:1 ,name: "5tart Travel",src: "https://res.cloudinary.com/dfktz8zkt/image/upload/v1728428007/cbey16iyadftprvqda3k.png" , alt: "Proyecto grupal: desarrollo de un marketplace especializado en la compra y venta de viajes. La plataforma permite a los usuarios explorar, publicar y adquirir experiencias de viaje, facilitando la conexión entre agencias y viajeros de manera eficiente e intuitiva.", href: "https://5tart-travel-front-w2ip.vercel.app/" },{id: 2, name: "Las Divas De Romi",src: "https://res.cloudinary.com/dfktz8zkt/image/upload/v1732223080/ynlk1fioljxs2krzlhq3.png", alt:"Es un eCommerce de productos y cursos online una plataforma que permite la compra de productos físicos y digitales, como productos, así como el acceso a cursos en línea en diversas áreas. Los usuarios pueden comprar de manera fácil y segura, con pagos electrónicos y acceso inmediato a los cursos. (EN PROCESO)", href: "https://personal-woad-phi.vercel.app/" },]
     setProductos([...data])  
   }, []);
 
@@ -51,8 +64,8 @@ export const Carrusel = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 9000,
-    prevArrow: <PrevArrow />, // Botón personalizado "Prev"
-    nextArrow: <NextArrow />, // Botón personalizado "Next"
+    prevArrow: <PrevArrow className="" onClick={() => {}} />, // Botón personalizado "Prev"
+    nextArrow: <NextArrow className="" onClick={() => {}} />, // Botón personalizado "Next"
     responsive: [
       {
         breakpoint: 1024,
